bc2b21e2e12e6482376d161915324d35eeba9ee7
Luke <lukeskywalker1105@msn.com>
2016-01-30 17:41:17 -0500

    Initial Commit.

---
+++ Main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Main.py: Main execution program"""
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from MainWindow import Ui_MainWindow
+from PyQt5.uic import compileUi
+
+import os.path
+__author__ = "Luke Zambella"
+__copyright__ = "Copyright 2016"
+
+
+def setup_ui():
+    ui_file = open("Qyoutube-dl_main.ui")
+    compileUi(uifile=ui_file, pyfile=open("MainWindow.py", 'w'))
+
+
+def __main__():
+    # Generates a UI every time in case its been changed
+    setup_ui()
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(window)
+    window.show()
+    sys.exit(app.exec_())
+
+__main__()
